f74a14a30018390f4dfc508411a78c6544580560
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-03-29 16:34:31 -0600

    Navlist de Angular Material

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: []
+  styles: [
+    `
+      .container{
+        margin: 10px
+      }
+    `
+  ]
 })
 export class HomeComponent {
   showFiller = false;
