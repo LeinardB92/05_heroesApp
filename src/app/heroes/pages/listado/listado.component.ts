192a01eff03679c9718b8ba6fa9cf4eb61cdc907
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-03-29 18:52:40 -0600

    Creación de heroes.service, mostrar en consola la información de los héroes con la ayuda de una petición

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: []
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit{
+
+  constructor(private heroesService: HeroesService) {}
+
+  ngOnInit(): void {
+    this.heroesService.getHeroes()
+    .subscribe(resp => console.log(resp))
+    
+  }
 
 }
