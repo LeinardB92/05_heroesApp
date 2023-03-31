e7d5f7f71472750a4480ae8ec94adb78541d934d
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-03-31 15:33:24 -0600

    Uso de targetas Material card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,24 @@
+/*Este módulo tiene la función de contener los elementos de la API Angular Material*/ 
+
 import { NgModule } from '@angular/core';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';  
+import {MatCardModule} from '@angular/material/card'; 
+import {MatDividerModule } from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon'; 
 import {MatListModule} from '@angular/material/list'; 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule
   ]
 })
 export class MaterialModule { }
