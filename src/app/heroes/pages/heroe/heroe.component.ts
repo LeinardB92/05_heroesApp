84fe953ffb75d6ed8d016ef2d2f6f997c181bf9d
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-04-04 07:03:06 -0600

    Ruta Heroe y Editar Heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: []
 })
-export class HeroeComponent {
+export class HeroeComponent implements OnInit{
+  
+  constructor(private activatedRoute: ActivatedRoute){}
 
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .subscribe(params => {
+      console.log(params['id']);
+    })  
+  }
 }
