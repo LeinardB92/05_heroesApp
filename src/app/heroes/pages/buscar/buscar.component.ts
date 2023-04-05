f9a141f1e13ab5a8241f351e9c214f2ab44bab08
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-04-04 22:26:19 -0600

    Autocomplete de Angular Material

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-buscar',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class BuscarComponent {
+  termino:string = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe;
 
+  constructor(private heroesService: HeroesService){}
+
+  buscando(){
+    this.heroesService.getSugerencias(this.termino)
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent){
+    const heroe: Heroe = event.option.value;
+    this.termino = heroe.superhero;
+
+    this.heroesService.getHeroePorId(heroe.id!)
+    .subscribe(heroe => this.heroeSeleccionado = heroe)
+  }
 }
