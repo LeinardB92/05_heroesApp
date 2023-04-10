d3615e839539b51df539dc93b543ff2e85b1c850
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-04-10 14:43:23 -0600

    AuthService, servicio para mantener el estado de la autenticación

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +9,25 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(private router: Router){}
+  constructor(private router: Router,
+              private authService : AuthService){}
 
   login(){
-    // <login> irá al backend.
+    // <login()> irá al backend con ayuda del servicio auth.service.ts., así que hay que inyectarlo.
+    this.authService.login().subscribe(resp => {
+      console.log(resp)
+
+      if(resp.id){
+        // Una vez teniendo la confirmación del backend de que el usuario existe, navegar a la pantalla de listado de Héroes, para ello ocuparemos inyectar a Router.
+        this.router.navigate(['./heroes']);
+      }
+    });
+
     // El backend debe de confirmar que existe un usuario.
     // Ese usuario debe de almacenarse en un servicio, para que esté disponible para toda la aplicación.
-    // Una vez teniendo esa información, navegar a la pantalla de listado de Héroes, para ello ocuparemos inyectar a Router.
     
-    this.router.navigate(['./heroes'])
+    
+    
 
   }
 }
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Auth } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseURL: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  login(){
+    return this.http.get<Auth>(`${this.baseURL}/usuarios/1`)
+  }
+}
