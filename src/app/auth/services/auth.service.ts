0ac247735173f995f6a2d4d4d68ce15fdcaea4b2
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-04-10 16:06:50 -0600

    Mostrar la información del usuario activo

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,16 @@ import { Auth } from '../interfaces/auth.interface';
 export class AuthService {
 
   private baseURL: string = environment.baseUrl;
+  private _auth!: Auth;
 
   constructor(private http: HttpClient) { }
 
+  get auth(){
+    return {...this._auth};
+  }
+
   login(){
     return this.http.get<Auth>(`${this.baseURL}/usuarios/1`)
+      .pipe(tap(auth => this._auth = auth))
   }
 }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth.interface';
+
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +17,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
  
-  constructor(private router: Router){}
+  constructor(private router: Router,
+              private authService: AuthService){}
+
+  get auth(): Auth{
+    return this.authService.auth;
+  }
 
   logout(){
     this.router.navigate(['./auth'])
